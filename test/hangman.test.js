6f21c0e3a8724d241e7f8dbcfe0e5adaaa574774
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-09-23 14:56:33 +0100

    Written test for secret word

--- test/hangman.test.js
+++ test/hangman.test.js
@@ -1,1 +1,10 @@
-const Hangman = require('../main/hangman')
+
+const Hangman = require('../main/hangman');
+
+beforeEach(() => {
+  game1 = new Hangman();
+})
+
+  test('User can input a secret word', () =>{
+    expect(game1.setSecretWord("FIRST")).toBe("FIRST")
+});
