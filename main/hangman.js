e1e776badf54d6938c7b605b095ac27f5a77bb86
Jack Hooper <77017281+crotchetycrow@users.noreply.github.com>
2021-09-23 15:44:54 +0100

    Added a setGuesses and startGame functions & tests

--- main/hangman.js
+++ main/hangman.js
@@ -1,10 +1,20 @@
 class Hangman {
   constructor() {
     this.secretWord = "";
+    this.guesses = 0;
+    this.isInProgress = false;
   }
   
   setSecretWord(word) {
     this.secretWord = word.toUpperCase();
+  }
+
+  setGuesses(numberOfGuesses) {
+    this.guesses = numberOfGuesses
+  }
+
+  startGame() {
+    this.isInProgress = true;
   }
 };
 
--- test/hangman.test.js
+++ test/hangman.test.js
@@ -5,7 +5,17 @@ beforeEach(() => {
   game1 = new Hangman();
 })
 
-  test('User can input a secret word', () =>{
+  test('User can input a secret word', () => {
     game1.setSecretWord("first");
     expect(game1.secretWord).toBe("FIRST");
+  });
+
+test('Users can set incorrect number of guesses', () => {
+  game1.setGuesses(3);
+  expect(game1.guesses).toBe(3);
+});
+
+test('Can check if a game is in progress', () => {
+  game1.startGame()
+  expect(game1.isInProgress).toBe(true);
 });
