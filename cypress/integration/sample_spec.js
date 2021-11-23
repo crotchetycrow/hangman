ba06a679ae6f50f62694f17636622eb2e48bb1ed
Inbar Vernia <inbar.vernia@gmail.com>
2021-11-23 20:35:57 +0000

    Added exception for module error in Cypress sample_spec file

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,3 +1,14 @@
+Cypress.on('uncaught:exception', (err, runnable) => {
+  // because we are exporting Hangman for Jest testing
+  // we expect 'ReferenceError: module is not defined'
+  // and don't want to fail the test so we return false
+  if (err.message.includes('module is not defined')) {
+    return false
+  }
+  // we still want to ensure there are no other unexpected
+  // errors, so we let them fail the test
+})
+
 describe('My first test', function () {
   it('Visits our hangman page', () => {
     cy.visit('../../main/index.html')
